c00f5f5704de4994da4ff2d88a8bdcfb9780a700
Robert Wagner <rwwagner90@gmail.com>
2020-08-28 14:57:36 -0400

    Remove path assertion to better align with `mut`
    `mut` was able to handle `undefined` values. This was very helpful for things like testing, where you may only want to define a few values, not everything for your whole app.

--- addon/helpers/set.js
+++ addon/helpers/set.js
@@ -1,5 +1,4 @@
 import { helper } from '@ember/component/helper';
-import { assert } from '@ember/debug';
 import { set as emberSet } from '@ember/object';
 
 function set(positional, named) {
@@ -15,14 +14,11 @@ function set(positional, named) {
     path = maybePath;
   }
 
-  assert(
-    'you must pass a path to {{set}}. You can pass a path statically, as in `{{set this.foo}}`, or with the path argument dynamically, as in `{{set this path="foo"}}`',
-    Boolean(target) && (typeof path === 'string' && path.length > 0 || typeof path === 'symbol' || typeof path === 'number')
-  );
-
-  return positional.length === 3
-    ? () => emberSet(target, path, maybeValue)
-    : value => emberSet(target, path, value);
+  if (path) {
+    return positional.length === 3
+      ? () => emberSet(target, path, maybeValue)
+      : value => emberSet(target, path, value);
+  }
 }
 
 export default helper(set);
