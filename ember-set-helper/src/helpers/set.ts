dfbe6147513bd2bd4c6050ec114727a2ac59526e
johanrd <johanroed@gmail.com>
2023-12-22 03:28:54 +0100

    convert to plain function as helper to get better context in template type checking

--- ember-set-helper/src/helpers/set.ts
+++ ember-set-helper/src/helpers/set.ts
@@ -1,20 +1,18 @@
-import { helper } from '@ember/component/helper';
 import { set as emberSet } from '@ember/object';
 import { assert } from '@ember/debug';
 
-function set<T extends object, K extends keyof T>(
-  positional: [T, K] | [T, K, T[K]],
+export default function set<T extends object, K extends keyof T & string>(
+  target: T,
+  path: K,
+  maybeValue?: T[K],
 ) {
-  const [target, path, maybeValue] = positional;
   assert(
     'you must pass a path to {{set}}. You can pass a path statically, as in `{{set this "foo"}}`, or with the path dynamically, as in `{{set this this.greetingPath "Hello"}}`',
     (typeof path === 'string' && path.length > 0) ||
       typeof path === 'symbol' ||
       typeof path === 'number',
   );
-  return positional.length === 3
+  return arguments.length === 3
     ? () => emberSet(target, path, maybeValue)
     : (value: unknown) => emberSet(target, path, value);
 }
-
-export default helper(set);
