a6c3b61b5442800ef582e6bfdf604f0371d67894
johanrd <johanroed@gmail.com>
2023-12-22 03:22:00 +0100

    allow both two and three arguments

--- ember-set-helper/src/helpers/set.ts
+++ ember-set-helper/src/helpers/set.ts
@@ -2,7 +2,9 @@ import { helper } from '@ember/component/helper';
 import { set as emberSet } from '@ember/object';
 import { assert } from '@ember/debug';
 
-function set<T extends object, K extends keyof T>(positional: [T, K, T[K]]) {
+function set<T extends object, K extends keyof T>(
+  positional: [T, K] | [T, K, T[K]],
+) {
   const [target, path, maybeValue] = positional;
   assert(
     'you must pass a path to {{set}}. You can pass a path statically, as in `{{set this "foo"}}`, or with the path dynamically, as in `{{set this this.greetingPath "Hello"}}`',
